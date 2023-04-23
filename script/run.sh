d1bfad9aa300fd9c247530b0edc917e2712ee8eb
qichanna <qichanna@126.com>
2023-04-23 13:50:10 +0800

    submit some configs
    submit some configs

--- script/run.sh
+++ script/run.sh
@@ -2,3 +2,4 @@
 # qemu-system-x86_64 -s -S -hda hd60M.img
 # 适用于mac
 qemu-system-i386  -m 128M -s -S  -drive file=hd60M.img,index=0,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
+# qemu-system-i386  -m 128M -drive file=hd60M.img,index=0,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
