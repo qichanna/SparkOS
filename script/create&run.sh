251af58f5728bb1e9a311dd2a6cdb9f4b6a0eedb
qichanna <qichanna@126.com>
2023-04-28 20:21:41 +0800

    fix loader image and show color
    fix loader image and show color

--- script/create&run.sh
+++ script/create&run.sh
@@ -10,5 +10,5 @@ sh ../script/write_img.sh
 #qemu-system-i386-hda hd60M.img
 #qemu-system-i386 -s -S -hda hd60M.img
 # 适用于mac
-#qemu-system-i386  -m 128M  -drive file=hd60M.img,index=0,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
-qemu-system-i386  -m 128M -s -S  -drive file=hd60M.img,index=0,media=disk,format=raw,id=hd0 -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
+qemu-system-i386  -m 128M  -drive file=hd60M.img,index=0,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
+#qemu-system-i386  -m 128M -s -S  -drive file=hd60M.img,index=0,media=disk,format=raw,id=hd0 -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
--- src/kernel/kernel_entry.c
+++ src/kernel/kernel_entry.c
@@ -27,13 +27,13 @@ void main(void)
 {
 //    cli();
     set_color_panel();
-     char* p = 0xa0000;
-     for (int i = 0; i <= 0xaffff; i++) {
+//     char* p = 0xa0000;
+//     for (int i = 0; i <= 0xaffff; i++) {
 //     	write_mem8(i, 15);
 //     	write_mem8(i, i & 0x0f);
-        *(p + i) = 15;
+//        *(p + i) = 10;
 //        *(p + i) = i & 0x0f;
-     }
+//     }
 
     char *vram;
     int xsize, ysize;
@@ -47,18 +47,19 @@ void main(void)
     boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
 
      boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
-//    boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
-//    boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
-//    boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
-//    boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
-//    boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
-//
-//    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
-//    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
-//    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
-//    boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+    boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+    boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+    boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+    boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+    boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
 
 
+    while (1);
      for (;;) {
      	io_hlt();
      }
